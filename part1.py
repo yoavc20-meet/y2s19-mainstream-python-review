8a5445c717c2b6191ed85a81d0a2a42c24d5c8ce
myname21-meet <myname21@meet.mit.edu>
2019-07-28 13:45:33 +0300

    labs

--- part1.py
+++ part1.py
@@ -1,13 +1,30 @@
 # Part 1 of the Python Review lab.
 
 def hello_world():
-	pass
+	print("hello world")
 
 def greet_by_name(name):
-	pass
+	name = input()
+	print("hello" + name)
 
-def encode(x):
-	pass
+def encode(x, y):
+	#global x
+ #	while( y <1 or y>250):
+ 	print("y")
+ 	y= input()
+ 	#while(x>100 or x<500) :
+ 	print("x")
+ #	global x
+ 	x = input()
+ 	global z
+ 	z = x+ 3953531
+ #	print(z)
+ 	if (1< y < 250 and 500<x<1000):
+ 		product = x*y
+ 		print(product)
+ 	else:
+ 		print("Invalid input: Outside range")
 
-def decode(y):
-	pass
+ 	def decode(y):
+		x = z - 3953531
+encode(d, f)
